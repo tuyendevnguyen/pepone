4fe48ba4fdf2e42f37d42a9874168ddda9f41c21
tuyendevnguyen <161902449+tuyendevnguyen@users.noreply.github.com>
2024-03-02 08:51:35 +0700

    Add files via upload

---
+++ mainjs.js
@@ -0,0 +1,34 @@
+/*đăng ký*/
+function clickclick() {
+    var modal = document.querySelector('.modal');
+    if (modal.style.display === "none") {
+        modal.style.display = "block"; 
+    } else {
+        modal.style.display = "none"; 
+    }
+}
+function back() {
+    var modal = document.querySelector('.modal');
+    if (modal.style.display === "block") {
+        modal.style.display = "none"; 
+    } else {
+        modal.style.display = "block"; 
+    }
+}
+/*đăng nhập*/
+function clickclick2() {
+    var modem= document.querySelector('.modem');
+    if (modem.style.display === "none") {
+        modem.style.display = "block"; 
+    } else {
+        modem.style.display = "none"; 
+    }
+}
+function back2() {
+    var modem = document.querySelector('.modem');
+    if (modem.style.display === "block") {
+        modem.style.display = "none"; 
+    } else {
+        modem.style.display = "block"; 
+    }
+}
